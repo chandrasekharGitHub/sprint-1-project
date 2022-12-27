ca31f261d97f6edb806a6ab319640bd622c7956f
chandrasekharGitHub <chandrasekhar.p.pasupuleti@outlook.com>
2022-12-27 10:46:40 +0530

    second

--- spring-data-jpa-project/src/main/java/com/spring/boot/demo/repository/EmployeeRepository.java
+++ spring-data-jpa-project/src/main/java/com/spring/boot/demo/repository/EmployeeRepository.java
@@ -1,8 +1,14 @@
 package com.spring.boot.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.boot.demo.model.Employee;
 
+
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long>  {	
+	public List<Employee> getAllEmployees();
+	
 }
