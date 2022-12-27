23e9e67e0c7f328ba6362d5175d0ee2e78bff3f2
chandrasekharGitHub <chandrasekhar.p.pasupuleti@outlook.com>
2022-12-27 10:49:08 +0530

    third

--- spring-data-jpa-project/src/main/java/com/spring/boot/demo/repository/EmployeeRepository.java
+++ spring-data-jpa-project/src/main/java/com/spring/boot/demo/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.spring.boot.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,5 +11,7 @@ import com.spring.boot.demo.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>  {	
 	public List<Employee> getAllEmployees();
+	
+	public Optional<Employee> findById(Long id);
 	
 }
